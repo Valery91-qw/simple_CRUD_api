ac26335066f0a8b18a0d8faa8a647a8ec0ada1a5
Valery91-qw <vakriv91@gmail.com>
2022-06-18 20:52:55 +0300

    feat: create parse body in POST

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -14,11 +14,20 @@ export class Controller {
     }
     static async createUser(req: IncomingMessage, res: ServerResponse): Promise<void> {
         try {
-
-            const newUser = await model.createUser({age: '22', username: "jon", hobbies: ['loot'] })
-
-            res.writeHead(201, {"Content-Type": "application/json"} )
-            res.end(JSON.stringify(newUser))
+            let data: string = '';
+            req.on("data", (chunk) => {
+               data += chunk.toString();
+            })
+            req.on("end", async () => {
+                if(data !== undefined) {
+                    const reqBody = JSON.parse(data);
+                    const newUser = await model.createUser(reqBody)
+                    res.writeHead(201, {"Content-Type": "application/json"} )
+                    res.end(JSON.stringify(newUser))
+                } else {
+                    return
+                }
+            })
         } catch (e) {
             console.warn(e)
         }
